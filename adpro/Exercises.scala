572bafdf18cf0c37390cd343ca395b07b7ab8ccc
sasudavid <34894599+sasudavid@users.noreply.github.com>
2020-09-14 00:47:55 +0200

    Update Exercises.scala

--- adpro/Exercises.scala
+++ adpro/Exercises.scala
@@ -1,15 +1,18 @@
 // Advanced Programming, A. Wąsowski, IT University of Copenhagen
 //
-// Group number: _____
+// Group name: fprog
 //
-// AUTHOR1: __________
-// TIME1: _____ <- how much time have you used on solving this exercise set
+// AUTHOR1: David Sasu
+// TIME1: > 6 hours <- how much time have you used on solving this exercise set
 // (excluding reading the book, fetching pizza, and going out for a smoke)
 //
-// AUTHOR2: __________
-// TIME2: _____ <- how much time have you used on solving this exercise set
+// AUTHOR2: Pelgrim Ionis Ernest Charraud
+// TIME2: > 6 hours <- how much time have you used on solving this exercise set
 // (excluding reading the book, fetching pizza, and going out for a smoke)
 //
+// AUTHOR3: Kristinn Heiðar Freysteinsson
+
+
 // You should work with the file by following the associated exercise sheet
 // (available in PDF from the course website).
 //
@@ -27,14 +30,21 @@ trait OrderedPoint extends scala.math.Ordered[java.awt.Point] {
 
   this: java.awt.Point =>
 
-  override def compare (that: java.awt.Point): Int =  ???
-
-}
+  override def compare (that: java.awt.Point): Int =  {
+    if((this.x < that.x) || (this.x == that.x && this.y < that.y))
+      -1
+    else if (this.x == that.x && this.y == that.y)
+      0
+    else
+      1
+    }
+  }
 
 // Try the following (and similar) tests in the repl (sbt console):
 // val p = new java.awt.Point(0,1) with OrderedPoint
 // val q = new java.awt.Point(0,2) with OrderedPoint
 // assert(p < q)
+
 
 sealed trait Tree[+A]
 case class Leaf[A] (value: A) extends Tree[A]
@@ -44,36 +54,54 @@ object Tree {
 
   // Exercise 2
 
-  def size[A] (t :Tree[A]): Int = ???
+  def size[A] (t :Tree[A]): Int =  t match{
+    case Leaf(x) => 1
+    case Branch(l, r) => 1 + size(l) + size(r)
+  }
 
   // Exercise 3
 
-  def maximum (t: Tree[Int]): Int = ???
+  def maximum (t: Tree[Int]): Int = t match{
+    case Leaf(x) => x
+    case Branch(l,r) => maximum(l) max maximum(r)
+  }
 
   // Exercise 4
 
-  def map[A,B] (t: Tree[A]) (f: A => B): Tree[B] = ???
+  def map[A,B] (t: Tree[A]) (f: A => B): Tree[B] = t match{
+    case Leaf(x) => Leaf(f(x))
+    case Branch(l,r) => Branch(map(l)(f), map(r)(f))                    
+  }
 
   // Exercise 5
+/*
+  def fold[A,B] (t: Tree[A]) (f: (B,B) => B) (g: A => B): B = t match{
+    case Leaf(x) => g(x)
+    case Branch(l,r) => f(fold(l)(f)(g), fold(r)(f)(g))
+  }
 
-  def fold[A,B] (t: Tree[A]) (f: (B,B) => B) (g: A => B): B = ???
+  def size1[A] (t: Tree[A]): Int = 
+    fold(t) ((l, r) => (1 + l + r)) (x => 1)
 
-  def size1[A] (t: Tree[A]): Int = ???
+  def maximum1 (t: Tree[Int]): Int = 
+    fold(t)(_ max _)(x => x)
 
-  def maximum1 (t: Tree[Int]): Int = ???
+  def map1[A,B] (t: Tree[A]) (f: A=>B): Tree[B] =
+    fold(t)(x => Leaf(f(x)): Tree[B])(Branch(_,_))
 
-  def map1[A,B] (t: Tree[A]) (f: A=>B): Tree[B] = ???
-
+    */
 }
+
 
 sealed trait Option[+A] {
 
   // Exercise 6
 
-  def map[B] (f: A=>B): Option[B] = this match {
+  def map[B] (f: A=>B): Option[B] =  this match {
     case Some (a) => Some(f(a))  
     case None => None
   }
+
 
   /**
    * Ignore the arrow (=>) in default's type below for now.
@@ -87,8 +115,8 @@ sealed trait Option[+A] {
     case Some(a) => a
   }
 
-  def flatMap[B] (f: A => Option[B]): Option[B] =
-  map(f) getOrElse None
+  def flatMap[B] (f: A => Option[B]): Option[B] = 
+    map(f) getOrElse None
 
   def filter (p: A => Boolean): Option[A] = this match {
     case Some(a) if p(a) => this
@@ -110,48 +138,29 @@ object ExercisesOption {
 
   // Exercise 7
 
-  def variance(xs: Seq[Double]): Option[Double] =
+  def variance (xs: Seq[Double]): Option[Double] = 
     mean(xs) flatMap (m => 
     mean(xs.map(x => 
     math.pow(x - m, 2))))
 
-
-/*   def variance (xs: Seq[Double]): Option[Double] = 
-  for {
-    x <- xs
-    xm = math.pow(x - mean(xs), 2)
-  } yield mean(xs) */
-
   // Exercise 8
 
-  def map2[A,B,C] (ao: Option[A], bo: Option[B]) (f: (A,B) => C): Option[C] =
-  for {
+  def map2[A,B,C] (ao: Option[A], bo: Option[B]) (f: (A,B) => C): Option[C] = 
+    for {
     a <- ao
     b <- bo
   } yield f(a, b)
 
-/*   ao flatMap (a => bo map (b => f(a, b)))
-flatMap enables the function to be option aware
-So now what your map2 does? It accepts 2 instances of Option monad and a function that transform "raw" pair of types into a single new "raw" type and 
-return Option of that result type. 
-Logically this is similar to map but to implement it you need a flatMap. 
-To call your f you need to "unpack" both ao and bo. But monad doesn't provide a way to just "unpack" raw value. 
-You might want to use just map. After all map2 is logically similar to it! However, if you write
-ao.map(aa =>
-  bo.map(bb => f(aa,bb)))
-as you might naively do, it wouldn't work as you might expect. The reason is that bo.map(bb => f(aa,bb)) returns Option[C] 
-(remember, there is no standard way to "unpack" a monad) and thus your function passed to ao.map(aa => ...) returns Option[C] and 
-thus the result would be Option[Option[C]]. But this exactly where flatMap comes to the rescue! It allows you "unpack" this double Option into a simple Option[C].
-Explanation Source : StackOverFlow
- */
-
   // Exercise 9
 
-  def sequence[A] (aos: List[Option[A]]): Option[List[A]] =
-   aos.foldRight[Option[List[A]]](Some(Nil))((x,y) => map2(x,y)(_ :: _))
+  def sequence[A] (aos: List[Option[A]]): Option[List[A]] = 
+    aos.foldRight[Option[List[A]]](Some(Nil))((x,y) => map2(x,y)(_ :: _))
 
   // Exercise 10
 
-  def traverse[A,B] (as: List[A]) (f :A => Option[B]): Option[List[B]] = ???
+  def traverse[A,B] (as: List[A]) (f :A => Option[B]): Option[List[B]] =    as match {
+      case Nil => Some(Nil)
+      case h::t => map2(f(h), traverse(t)(f))(_ :: _)
+    }
 
 }
